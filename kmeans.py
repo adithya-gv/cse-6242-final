5a9143120f1b535c99b354ae4feead08d0006652
Adithya Vasudev <adithya.gvasudev@gmail.com>
2024-11-23 16:40:59 -0500

    kmeans clear

--- kmeans.py
+++ kmeans.py
@@ -75,5 +75,3 @@ def live_clustering():
     cleaned_data_with_names = clean_data_with_names(game_data_with_gvi)
     cleaned_data_with_names['cluster'] = cluster_labels
     return cleaned_data_with_names
-
-generate_cluster_dataset('data/final_game_data.csv')
